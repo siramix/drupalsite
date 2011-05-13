c42d94e76035136b623d8d717fc34be14e7f47f3
LucasRowe <lucas.rowe@gmail.com>
2011-05-13 00:14:34 -0400

    DPL: Added CSS for app grid and edited javascript to be dynamic across environments.

--- grocerygetter/node--appgrid.tpl.php
+++ grocerygetter/node--appgrid.tpl.php
@@ -1,8 +1,13 @@
+<?php 
+global $base_url 
+?>
+
 <script type="text/javascript">
 	function disable(){
 	links=document.getElementsByTagName('A');
 	for(var i=0; i<links.length; i++) {
-	if (links[i] == "http://siramix.localhost/?q=node/15")
+	if (links[i] == "<?php print($base_url); ?>/?q=moretocome" || 
+	    links[i] == "<?php print($base_url); ?>/moretocome" )
 	{
 	links[i].href="javascript:return false";
 	}
