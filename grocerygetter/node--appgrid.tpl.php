6fb46992eae6536fcdb954e2749306a72f4ad410
LucasRowe <lucas.rowe@gmail.com>
2011-05-20 02:16:26 -0400

    CSS: Menu tweaks. CLN: HTML code now validates and javascript shouldnt error in IE.

--- grocerygetter/node--appgrid.tpl.php
+++ grocerygetter/node--appgrid.tpl.php
@@ -1,18 +1,19 @@
 <?php 
-global $base_url 
+global $base_url
 ?>
 
 <script type="text/javascript">
-	function disable(){
-	links=document.getElementsByTagName('A');
-	for(var i=0; i<links.length; i++) {
-	if (links[i] == "<?php print($base_url); ?>/?q=moretocome" || 
-	    links[i] == "<?php print($base_url); ?>/moretocome" )
-	{
-	links[i].href="javascript:return false";
-	}
-	   }
+/* <![CDATA[ */
+	function disable() {
+		links=document.getElementsByTagName('A');
+		for(var i=0; i<links.length; i++) {
+			if (links[i] == "<?php print($base_url); ?>/?q=moretocome" || 
+					links[i] == "<?php print($base_url); ?>/moretocome" ) {
+				links[i].href="#";
+			}
+		}
 	}
 
 	window.onload=disable;
+/* ]]> */
 </script>
